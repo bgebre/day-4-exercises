6485f656d2c1ea6b8a7ccc1949e737059806e941
Banna Gebre <bgebre09@gmail.com>
2014-07-24 21:46:23 +0000

    php connected yay

--- population.php
+++ population.php
@@ -1,27 +1,27 @@
-<?php
-$city = $_GET['city'];
-?>
 <h1>Welcome to Population Data Online</h1>
-<?
-$cities = array(
-  'Philadelphia' => 1548000,
-    'New York' => 8337000,
-      'Los Angeles' => 3858000,
-        'Seattle' => 634535,
-          'Boston' => 636479,
-          );
-if ($city && isset($cities[$city])) {
-    print '<p>The population of <strong>' . $city . '</strong> is <strong>' . $cities[$city] . '</strong></p>';
+<?php
+// Create connection
+// Can save arguments for mysql_connect() function as variables,
+// or input "manually" into the function.
+// e.g., mysql_connect('website.com', 'Jones', 'un1c0rn')
+$connection = mysql_connect('localhost', 'root', "")
+  or die("Unable to connect to $localhost: " . mysql_errno());
+
+// Select database
+$selected_db = mysql_select_db('day4exercises')
+  or die("Unable to select database: 'day4exercises': " . mysql_errno());
+
+// SQL Query
+$result = mysql_query("SELECT city_name, population FROM population");
+
+// Fetch the data from the result
+while ($row = mysql_fetch_array($result)){
+  echo "City: " . $row['city_name'] . "<br>" ."Population "  . $row['population']  . "<br>";
 }
- 
- ?>
- <h2>Our Cities</h2>
- <ul>
- <?php
-   foreach ($cities as $name => $value) {
-         print '<li><a href="/jody/population.php?city=' . $name  . '">' . $name
-             . '</a></li>';
-               }
-               ?>
-               </ul>
+
+
+// Close connection
+mysql_close($connection);
+?>
+
 
